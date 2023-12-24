e91d41431ec4cc1eecdab004dcc74da8cc0c74f7
FurqanEmam <furqanemam@gmail.com>
2023-12-25 00:16:11 +0600

    services component created

---
+++ src/pages/Home/Services/Services.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div>
+      <div className="text-center space-y-5">
+        <h3 className="text-orange-500 font-bold text-xl">Services</h3>
+        <h2 className="text-5xl text-[#151515)]">Our Services Area</h2>
+        <p className="">
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised <br /> words which do not look even slightly
+          believable.{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
