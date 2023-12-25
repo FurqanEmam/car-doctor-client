489930510f04b8cf6cf7ffaead9c25802befa705
FurqanEmam <furqanemam@gmail.com>
2023-12-25 09:12:36 +0600

    serviceCard added

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ServiceCard from "./ServiceCard";
 
 const Services = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
   return (
     <div>
       <div className="text-center space-y-5">
@@ -10,6 +18,13 @@ const Services = () => {
           the majority have suffered alteration in some form, by injected
           humour, or randomised <br /> words which do not look even slightly
           believable.{" "}
+        </p>
+      </div>
+      <div>
+        <p className="grid lg:grid-cols-3 md:grid-cols-2 gap-4">
+          {services.map((service) => (
+            <ServiceCard key={service._id} service={service}></ServiceCard>
+          ))}
         </p>
       </div>
     </div>
